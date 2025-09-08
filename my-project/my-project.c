4e76b8db7e7bdbcf02cde35088373a6718449f35
Arbi <arbindarizki@gmail.com>
2025-09-08 17:07:36 +0700

    add button

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -3,17 +3,26 @@
 
 int main(void) {
 	rcc_periph_clock_enable(RCC_GPIOC);
-
+	rcc_periph_clock_enable(RCC_GPIOB);
+	// Led setup
 	gpio_set_mode(GPIOC, 
 			GPIO_MODE_OUTPUT_2_MHZ,
 			GPIO_CNF_OUTPUT_PUSHPULL,
 			GPIO13);
+	
+	// Button setup
+	gpio_set_mode(GPIOB,
+			GPIO_MODE_INPUT,
+			GPIO_CNF_INPUT_PULL_UPDOWN,
+			GPIO12);
 
 	while(1) {
-		for (int i = 0; i < 1000000; i++) {
-			__asm__("nop");
-		}
+		if (gpio_get(GPIOB, GPIO12)){
+			for (int i = 0; i < 1000000; i++) {
+				__asm__("nop");
+			}
 
-		gpio_toggle(GPIOC, GPIO13);
+			gpio_toggle(GPIOC, GPIO13);
+		}
 	}
 }
